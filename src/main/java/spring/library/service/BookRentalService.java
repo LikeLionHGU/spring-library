4173e7762497b666bf58f8196d738c99b5d57900
7SH7 <kimseunghwan7777@gmail.com>
2024-02-01 04:17:44 +0900

    refactor: 특정 인물이 현재 대여하고 있는 내역 출력하는 메서드에서 터미널에 출력해주는 코드 삭제

--- src/main/java/spring/library/service/BookRentalService.java
+++ src/main/java/spring/library/service/BookRentalService.java
@@ -76,15 +76,11 @@ public class BookRentalService {
 	public List<RentalManagement> showRentalBookList(Long memberId){
 		if (!memberRepository.existsById(memberId)) { throw new IllegalArgumentException("해당 멤버의 기록이 없습니다."); }
 		List<RentalManagement> firstCheckBooks = bookHistoryRepository.findByRentMemberId(memberId);
-    for (RentalManagement book : firstCheckBooks) System.out.println(book);
-		System.out.println("----------------");
-    System.out.println("----------------");
 		List<RentalManagement> rentedBooks = new ArrayList<>(); ;
 		for(RentalManagement book : firstCheckBooks){
 			boolean isRental = book.getStatus().equals("대출 중");
 			if(isRental) rentedBooks.add(book);
 		}
-		System.out.println("----------------");
 
 		return rentedBooks;
 
